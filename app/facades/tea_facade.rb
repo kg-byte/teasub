127b2675ba9096891b4a05c72ab24f05a583d122
Kim G <xiaole.guo@yahoo.com>
2022-08-02 17:30:00 -0600

    refactor tea facade

--- app/facades/tea_facade.rb
+++ app/facades/tea_facade.rb
@@ -2,38 +2,16 @@ class TeaFacade
 	def self.QTea_selection(subscription_id)
 		data = [get_tea.sample]
 		generate_tea(subscription_id, data)
-		
-		# Tea.create(title: data[:name], 
-		# 			description: data[:description], 
-		# 			temperature: data[:temperature], 
-		# 			brew_time: data[:brew_time], 
-		# 			subscription_id: subscription_id)
 	end
 
 	def self.plenTea_selection(subscription_id)
 		data = get_tea.shuffle[0..1]
 		generate_tea(subscription_id, data)
-
-		# data.map do |datum|
-		# 	Tea.create(title: datum[:name], 
-		# 			description: datum[:description], 
-		# 			temperature: datum[:temperature], 
-		# 			brew_time: datum[:brew_time], 
-		# 			subscription_id: subscription_id)
-		# end
-
 	end
 
 	def self.thirsTea_selection(subscription_id)
 		data = get_tea.shuffle[0..3]
 		generate_tea(subscription_id, data)
-		# data.map do |datum|
-		# 	Tea.create(title: datum[:name], 
-		# 			description: datum[:description], 
-		# 			temperature: datum[:temperature], 
-		# 			brew_time: datum[:brew_time], 
-		# 			subscription_id: subscription_id)
-		# end
 	end
 
 
