89252cbb8c97ae05ba6cd34bf61afd4d3c9196ea
Kim G <xiaole.guo@yahoo.com>
2022-08-03 09:13:57 -0600

    refactor subscription controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,12 +3,7 @@ class SubscriptionsController < ApplicationController
 	include UpdateSubscriptionEdgeCaseHelper
 	def create
 		return new_edge_case_response if new_edge_case_conditions
-		subscription = Subscription.create(customer_id: params[:customer_id],
-							title: params[:subscription_type],
-							price: params[:subscription_type],
-							frequency: params[:subscription_type],
-							status:0
-							)
+		subscription = new_subscription(params[:customer_id], params[:subscription_type])
 		add_tea(subscription.title, subscription.id)
 		render json: SubscriptionSerializer.new(subscription)
 	end
@@ -16,18 +11,28 @@ class SubscriptionsController < ApplicationController
 	def update
 		return update_edge_case_response if update_edge_case_conditions
 		subscription = Subscription.find(params[:subscription_id])
-		if params[:new_status] == 'cancel'
-			subscription.update(status: 1)
-		elsif params[:new_status] == 'reactivate'
-			subscription.update(status: 0)
-		end
+		update_subscription(subscription, params[:new_status])
 		render json: SubscriptionSerializer.new(subscription)
 	end
 
-	def index
+	private
+	def new_subscription(customer_id, subscription_type)
+		Subscription.create(customer_id: customer_id,
+									title: subscription_type,
+									price: subscription_type,
+									frequency: subscription_type,
+									status:0
+									)
 	end
 
-	private
+	def update_subscription(subscription, new_status)
+		if new_status == 'cancel'
+			subscription.update(status: 1)
+		elsif new_status == 'reactivate'
+			subscription.update(status: 0)
+		end
+	end
+
 	def add_tea(subscription_type, subscription_id)
 		if subscription_type == "QTea"
 			TeaFacade.QTea_selection(subscription_id)
