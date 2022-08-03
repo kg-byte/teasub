da136e692f8f0cb9845d3143e8b23106ee7f77cc
Kim G <xiaole.guo@yahoo.com>
2022-08-02 18:40:01 -0600

    add sad path testing to handle edge cases when creating subscription

--- spec/requests/subscriptions_request_spec.rb
+++ spec/requests/subscriptions_request_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe Subscription, type: :request do
         'Accept': 'application/json'
       }
       post '/subscriptions', headers: headers, params: JSON.generate(params)
+      expect(response).to be_successful
       result = JSON.parse(response.body, symbolize_names: true)[:data]
 
       expect(result).to be_a(Hash)
@@ -25,7 +26,71 @@ RSpec.describe Subscription, type: :request do
       expect(result[:attributes][:customer_id]).to eq(user1.id)
       expect(result[:relationships]).to have_key(:tea)
       expect(result[:relationships][:tea][:data].count).to eq(1)
+    end
+  end
 
+  describe 'sad path' do
+    it 'cannot create subscription with missing params' do
+      params = {
+        "customer_id": '',
+        "subscription_type": 0
+      }
+      headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+      post '/subscriptions', headers: headers, params: JSON.generate(params)
+
+      expect(response.status).to eq(400)
+      result = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(result[:error]).to eq('Parameters cannot be empty')
+    end
+
+    it 'cannot create subscription with empty params' do
+      params = {
+        "subscription_type": 0
+      }
+      headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+      post '/subscriptions', headers: headers, params: JSON.generate(params)
+      
+      expect(response.status).to eq(400)
+      result = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(result[:error]).to eq('Both customer_id and subscription_type parameters are required')
+    end
+
+    it 'cannot create subscription with invalid subscription_type' do
+      params = {
+        "customer_id": user1.id,
+        "subscription_type": 4
+      }
+      headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+      post '/subscriptions', headers: headers, params: JSON.generate(params)
+      
+      expect(response.status).to eq(400)
+      result = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(result[:error]).to eq('subscription_type must be 0(QTea), 1(plenTea), or 2(thirsTea)')
+    end
+
+    it 'cannot create subscription with invalid customer_id' do
+      params = {
+        "customer_id": 0,
+        "subscription_type": 2
+      }
+      headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+      post '/subscriptions', headers: headers, params: JSON.generate(params)
+      
+      expect(response.status).to eq(400)
+      result = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(result[:error]).to eq('Invalid customer_id')
     end
   end
 end
